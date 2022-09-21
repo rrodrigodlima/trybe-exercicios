1a2796faf6dc9ab053579cd80c23d295feab1731
rrodrigodlima <rdrigo019@gmail.com>
2022-09-21 17:24:26 -0300

    operadores

---
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,34 @@
+let a = 3;
+let b = 5;
+let c = 7;
+
+//---
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//-----
+
+
+if (a > b) {
+    console.log("a é maior");
+ }
+else if (b > a) {
+    console.log("b é maior");
+}
+
+//---
+
+if (a > b && a > c) {
+    console.log(a);
+}
+else if (b > a && b > c) {
+    console.log(b);
+}
+else if (c > a && c > b) {
+    console.log(c);
+}
+
