86ce29e322328000c806466a93981ab5b267a77b
rrodrigodlima <rdrigo019@gmail.com>
2022-11-08 00:41:15 -0300

    exercise 7

--- fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise7.js
+++ fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise7.js
@@ -4,6 +4,6 @@ const authorWith3DotsOnName =
   books.find((book) => (
     book.author.name[1] === '.'
     && book.author.name[4] === '.'
-    & book.author.name[7] === '.'
+    && book.author.name[7] === '.'
   )).name;
 console.log(authorWith3DotsOnName);
