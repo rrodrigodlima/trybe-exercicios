1a779e3276192ab7d443ff2ce865c286a105314a
rrodrigodlima <rdrigo019@gmail.com>
2022-11-07 23:49:58 -0300

    exercise2

--- fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -1,5 +1,5 @@
 const books = require('./biblioteca');
 
-const file = books.map((book) => 
+const formatedBookNames = books.map((book) => 
 `${book.name} - ${book.genre} - ${book.author.name}`);
-console.log(file);
+console.log(formatedBookNames);
