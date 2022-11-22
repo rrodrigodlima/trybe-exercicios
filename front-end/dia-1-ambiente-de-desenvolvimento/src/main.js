b4459dc1c95977b666f030cb1530123d135fdd76
rrodrigodlima <rdrigo019@gmail.com>
2022-11-22 11:02:37 -0300

    Final commit

--- front-end/dia-1-ambiente-de-desenvolvimento/src/main.js
+++ front-end/dia-1-ambiente-de-desenvolvimento/src/main.js
@@ -1,4 +1,5 @@
-import validator from "validator";
+/* eslint-disable no-unused-expressions */
+import validator from 'validator';
 
 const inputArea = document.getElementById('textArea');
 const dropdown = document.getElementById('dropdown');
@@ -10,11 +11,22 @@ const result = document.querySelector('h2');
 validateBtn.addEventListener('click', () => {
   if (dropdown.selectedIndex === 0) {
     const validateEmail = validator.isEmail(`${inputArea.value}`);
-    validateEmail ? result.innerText='✅' : result.innerText='❌';
-    console.log('oi');
+    validateEmail ? result.innerText = '✅' : result.innerText = '❌';
+  }
+  if (dropdown.selectedIndex === 1) {
+    const validateCreditCard = validator.isCreditCard(`${inputArea.value}`);
+    validateCreditCard ? result.innerText = '✅' : result.innerText = '❌';
+  }
+  if (dropdown.selectedIndex === 2) {
+    const validateBtc = validator.isBtcAddress(`${inputArea.value}`);
+    validateBtc ? result.innerText = '✅' : result.innerText = '❌';
+  }
+  if (dropdown.selectedIndex === 3) {
+    const validateEth = validator.isEthereumAddress(`${inputArea.value}`);
+    validateEth ? result.innerText = '✅' : result.innerText = '❌';
+  }
+  if (dropdown.selectedIndex === 4) {
+    const validateIBAN = validator.isIBAN(`${inputArea.value}`);
+    validateIBAN ? result.innerText = '✅' : result.innerText = '❌';
   }
 });
-
-
-
-console.log(validator.isEmail('oi@gmail.com'));
