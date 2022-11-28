846f978151c5000ea1c68fac753e71e52d8c83c5
rrodrigodlima <rdrigo019@gmail.com>
2022-11-28 01:55:41 -0300

    swal

---
+++ front-end/secao-1-introducao-ao-front-end-e-javascript-assincrono/dia-4-async-await-e-testes-assincronos/src/main.js
@@ -0,0 +1,24 @@
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const preEl = document.querySelector('pre');
+import Swal from 'sweetalert2';
+
+buttonEl.addEventListener('click', handleClick);
+
+async function handleClick() {
+  const cep = inputEl.value;
+
+  try {
+    const result = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await result.json();
+
+    preEl.innerHTML = JSON.stringify(data);
+    return data;
+  } catch (error)  {
+    return Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: `${error.message}`,
+    })
+  }
+};
